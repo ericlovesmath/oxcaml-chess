df931af46caa50492c19d5112b58bdc1cf9f6059
Eric Lee <me@ericchanlee.com>
2026-08-28 09:26:58 -0400

    feat: add debug build option, zero_alloc bitboards

--- lib/bitboard.mli
+++ lib/bitboard.mli
@@ -4,7 +4,7 @@ type t = int64#
 
 val empty : t
 val full : t
-val of_square : rank:int -> file:int -> t
-val union : t -> t -> t
-val inter : t -> t -> t
-val mem : t -> rank:int -> file:int -> bool
+val of_square : rank:int -> file:int -> t [@@zero_alloc strict]
+val union : t -> t -> t [@@zero_alloc strict]
+val inter : t -> t -> t [@@zero_alloc strict]
+val mem : t -> rank:int -> file:int -> bool [@@zero_alloc strict]
